18469c309e61868b4223b77d83d5780545cfb6cc
nitzan <nitzantomer1998@gmail.com>
2023-04-03 18:59:59 +0300

    Eslint Prettier config

--- src/index.js
+++ src/index.js
@@ -1,21 +1,21 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const bodyParser = require('body-parser');
+const bodyParser = require("body-parser");
 
 app.use(bodyParser.json());
 
-app.get('/api'),
+app.get("/api"),
   (req, res) => {
-    console.log('Here');
+    console.log("Here");
   };
 
-app.post('/api/login', (req, res) => {
+app.post("/api/login", (req, res) => {
   const email = req.body.email;
   const password = req.body.password;
   // Here you can add your own authentication logic.
   // For example, you can query a database to see if the email and password match a user record.
 
-  if (email === 'example@gmail.com' && password === 'password123') {
+  if (email === "example@gmail.com" && password === "password123") {
     res.json({ valid: true });
   } else {
     res.json({ valid: false });
@@ -23,7 +23,7 @@ app.post('/api/login', (req, res) => {
 });
 
 // Define the API endpoint for handling sign-up submissions
-app.post('/api/register', (req, res) => {
+app.post("/api/register", (req, res) => {
   const { name, email, password } = req.body;
 
   // Perform any necessary validation on the form data
@@ -37,5 +37,5 @@ app.post('/api/register', (req, res) => {
 });
 
 app.listen(5000, () => {
-  console.log('Server start at port 5000');
+  console.log("Server start at port 5000");
 });
