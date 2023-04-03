fa943808d9dd61ff5c389a6607e8a34720eea269
nitzan <nitzantomer1998@gmail.com>
2023-04-03 21:12:58 +0300

    Fixing

--- src/index.js
+++ src/index.js
@@ -1,34 +1,14 @@
 const express = require("express");
 const cors = require("cors");
 
-const PORT = 3001;
+const PORT = process.env.PORT || 4000;
 const app = express();
 
 app.use(cors());
 
 app.get("/", (req, res) => {
-  console.log("Hey");
-});
-
-app.get("/api"),
-  (req, res) => {
-    console.log("Here");
-  };
-
-app.post("/api/login", (req, res) => {
-  const email = req.body.email;
-  const password = req.body.password;
-
-  if (email === "example@gmail.com" && password === "password123") {
-    res.json({ valid: true });
-  } else {
-    res.json({ valid: false });
-  }
-});
-
-app.post("/api/register", (req, res) => {
-  const { name, email, password } = req.body;
-  res.json({ success: true });
+  res.send(JSON.stringify("<h3>Hi From Backend API</h3>"));
+  return res;
 });
 
 app.listen(PORT, () => {
