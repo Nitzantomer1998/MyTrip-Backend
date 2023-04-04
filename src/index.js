24ceb510dbe5304e5a760715edc8ab6b4a6180e0
nitzan <nitzantomer1998@gmail.com>
2023-04-04 13:57:12 +0300

    Adding circle ci config

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
-import express from "express";
+import express from 'express';
 
 const PORT = process.env.port || 3001;
 const APP = express();
 
-APP.get("/", (req, res) => {
-  console.log("A new request has arrived to index.js");
-  res.send("Hello from the server main page");
+APP.get('/', (req, res) => {
+  console.log('A new request has arrived to index.js');
+  res.send('Hello from the server main page');
 });
 
 APP.listen(PORT, () => {
