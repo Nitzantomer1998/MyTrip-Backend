c3e9875efea355033745508701ec889c93a762db
nitzan <nitzantomer1998@gmail.com>
2023-04-07 17:16:02 +0300

    adding login post form

--- src/index.js
+++ src/index.js
@@ -1,30 +1,54 @@
 import express from 'express';
 import mongoose from 'mongoose';
-import dotenv from 'dotenv';
-import registerRouter from './routes/registerRouter.js';
-import loginRouter from './routes/loginRouter.js';
+import Register from './models/registerModel.js';
 
-dotenv.config();
-
+const PORT = process.env.PORT || 3001;
 const app = express();
-const PORT = process.env.PORT;
 
-// Middleware
 app.use(express.json());
 
-// Routes
-app.use('/register', registerRouter);
-app.use('/login', loginRouter);
+app.get('/', (req, res) => {
+  console.log('A new request has arrived to index.js');
+  res.send('Hello from the server main page');
+});
 
-// MongoDB connection
+app.post('/register', async (req, res) => {
+  try {
+    const register = await Register.create(req.body);
+    res.status(200).json(register);
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).json({ message: error.message });
+  }
+});
+
+app.post('/login', async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = await Register.findOne({ email });
+    if (!user) {
+      res.status(401).json({ message: 'Invalid credentials' });
+      return;
+    }
+    if (user.password !== password) {
+      res.status(401).json({ message: 'Invalid credentials' });
+      return;
+    }
+    res.status(200).json({ message: 'Login successful' });
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).json({ message: 'Server error' });
+  }
+});
+
 mongoose
-  .connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true,
-  })
+  .connect(
+    'mongodb+srv://nitzantomer1998:nitzantomer1998@backend-api.nminrxb.mongodb.net/?retryWrites=true&w=majority',
+    { useNewUrlParser: true, useUnifiedTopology: true }
+  )
   .then(() => {
     console.log('Connected to MongoDB');
+
     app.listen(PORT, () => {
       console.log(`Server is up and running at port --> ${PORT}`);
     });
