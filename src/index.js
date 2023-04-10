3791d734481354eb6beddcc0c7e1dfeadd0b4f88
nitzan <nitzantomer1998@gmail.com>
2023-04-10 10:24:28 +0300

    Adding cors package

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import dotenv from 'dotenv';
-
+import cors from 'cors';
 import connectToDB from './config/database.js';
 import userRoute from './routes/userRoute.js';
 
@@ -10,6 +10,11 @@ connectToDB();
 
 const app = express();
 
+app.use(
+  cors({
+    origin: 'https://project-management-5r3r.onrender.com',
+  })
+);
 app.use(express.json());
 app.get('/', (req, res) => {
   res.send('Default message');
