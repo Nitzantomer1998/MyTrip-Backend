b52b21f99d3a7c81361f49474398395d63e16689
nitzan <nitzantomer1998@gmail.com>
2023-04-20 08:29:21 +0300

    Updating database structure

--- src/config/database.js
+++ src/config/database.js
@@ -1,17 +1,17 @@
 import mongoose from 'mongoose';
 
-
 const connectToDB = async () => {
+  const options = {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  };
+
   try {
-    const mongooseConnection = await mongoose.connect(process.env.MONGODB_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
-    console.log('MongoDB Connected');
-    return mongooseConnection;
+    await mongoose.connect(process.env.DATABASE_URL, options);
+    console.log('Database connected');
   } catch (error) {
-    console.error(`Error --> ${error.message}`);
-    process.exit(1);
+    console.error(`Error connecting to database: ${error.message}`);
+    throw error;
   }
 };
 
