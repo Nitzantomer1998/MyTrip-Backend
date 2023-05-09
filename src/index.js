8e8947e5f8f4f0a4262d91d0e96440b5070cc3cb
Nitzan <nitzantomer1998@gmail.com>
2023-05-09 15:38:28 +0300

    Updating index.js (name and structure)

---
+++ src/index.js
@@ -0,0 +1,31 @@
+// Import needed packages
+import fileUpload from 'express-fileupload';
+import express from 'express';
+import dotenv from 'dotenv';
+import cors from 'cors';
+import { readdirSync } from 'fs';
+
+// Import needed files
+import connectDB from './config/database.js';
+
+// Config environment variables
+dotenv.config();
+
+// Connect to database
+await connectDB();
+
+// Connect to express
+const app = express();
+
+// Define express middlewares
+app.use(express.json());
+app.use(cors({ origin: process.env.BASE_URL, credentials: true }));
+app.use(fileUpload({ useTempFiles: true }));
+
+// Define Routes
+readdirSync('./src/routes').map(async (route) =>
+  app.use('/', (await import(`./routes/${route}`)).default)
+);
+
+// Define server listening port
+app.listen(process.env.PORT, () => console.log('Server Connected'));
