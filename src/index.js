6f2e94901fd07dfe2284ff63af25d66ecd7a8a77
Nitzan <nitzantomer1998@gmail.com>
2023-05-10 10:47:31 +0300

    Updating index.js structure

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 // Import needed packages
-import fileUpload from 'express-fileupload';
-import express from 'express';
-import dotenv from 'dotenv';
 import cors from 'cors';
+import dotenv from 'dotenv';
+import express from 'express';
+import fileUpload from 'express-fileupload';
 import { readdirSync } from 'fs';
 
 // Import needed files
