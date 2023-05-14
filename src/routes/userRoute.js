719238105ca06752aecaf43c243bfa70ce4539cb
Nitzan Tomer <Nitzantomer1998@gmail.com>
2023-05-14 18:52:22 +0300

    Updating the route followUser

--- src/routes/userRoute.js
+++ src/routes/userRoute.js
@@ -28,7 +28,7 @@ router.post('/userLogin', userLogin);
 router.post('/searchUser/:searchTerm', userMiddleware, searchUser);
 
 // Define PUT routes
-router.put('/followUser/:id', userMiddleware, followUser); // Works, but not perfect ----- update after fix following/follow button
+router.put('/followUser/:id', userMiddleware, followUser);
 router.put('/unfollowUser/:id', userMiddleware, unfollowUser); // Works, but not perfect ----- update after fix following/follow button
 router.put('/addUserToSearchHistory', userMiddleware, addUserToSearchHistory); // Works, but not perfect
 router.put('/removeUserFromSearch', userMiddleware, removeUserFromSearch); // Finished
