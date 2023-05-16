c9a656b40c3cab90e0b83098d5fe8398c602fc74
Nitzan Tomer <Nitzantomer1998@gmail.com>
2023-05-16 12:04:36 +0300

    Adjusting notes

---
+++ src/controllers/reactionController.js
@@ -0,0 +1,99 @@
+// Import needed models
+import Reaction from '../models/reactionModel.js';
+import User from '../models/userModel.js';
+
+// Import neede packages
+import mongoose from 'mongoose';
+
+async function getPostReactions(req, res) {
+  try {
+    const reactsArray = await Reaction.find({ postRef: req.params.id });
+
+    /*
+    const check1 = reacts.find(
+      (x) => x.reactBy.toString() == req.user.id
+    )?.react;
+    */
+    const newReacts = reactsArray.reduce((group, react) => {
+      let key = react['react'];
+      group[key] = group[key] || [];
+      group[key].push(react);
+      return group;
+    }, {});
+
+    const reacts = [
+      {
+        react: 'like',
+        count: newReacts.like ? newReacts.like.length : 0,
+      },
+      {
+        react: 'love',
+        count: newReacts.love ? newReacts.love.length : 0,
+      },
+      {
+        react: 'haha',
+        count: newReacts.haha ? newReacts.haha.length : 0,
+      },
+      {
+        react: 'sad',
+        count: newReacts.sad ? newReacts.sad.length : 0,
+      },
+      {
+        react: 'wow',
+        count: newReacts.wow ? newReacts.wow.length : 0,
+      },
+      {
+        react: 'angry',
+        count: newReacts.angry ? newReacts.angry.length : 0,
+      },
+    ];
+
+    const check = await Reaction.findOne({
+      postRef: req.params.id,
+      reactBy: req.user.id,
+    });
+    const user = await User.findById(req.user.id);
+    const checkSaved = user?.savedPosts.find(
+      (x) => x.post.toString() === req.params.id
+    );
+    res.json({
+      reacts,
+      check: check?.react,
+      total: reactsArray.length,
+      checkSaved: checkSaved ? true : false,
+    });
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+}
+
+async function postReaction(req, res) {
+  try {
+    const { postId, react } = req.body;
+    const check = await Reaction.findOne({
+      postRef: postId,
+      reactBy: req.user.id,
+    });
+    if (check == null) {
+      const newReact = new Reaction({
+        react: react,
+        postRef: postId,
+        reactBy: req.user.id,
+      });
+      await newReact.save();
+    } else {
+      if (check.react == react) {
+        await Reaction.findByIdAndRemove(check._id);
+      } else {
+        await Reaction.findByIdAndUpdate(check._id, {
+          react: react,
+        });
+      }
+    }
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+}
+
+// Export the functions
+export { getPostReactions, postReaction };
