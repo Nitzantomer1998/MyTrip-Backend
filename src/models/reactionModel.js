281cc440554646ac130e7514ec3e7ac28429b7d6
Nitzan Tomer <Nitzantomer1998@gmail.com>
2023-05-16 12:07:41 +0300

    Adjusting notes

---
+++ src/models/reactionModel.js
@@ -0,0 +1,24 @@
+// Import needed functions
+import { Schema, ObjectId, model } from 'mongoose';
+
+// Create reaction schema
+const reactionSchema = Schema({
+  react: {
+    type: String,
+    enum: ['like', 'love', 'haha', 'sad', 'angry', 'wow'],
+    required: true,
+  },
+  postRef: {
+    type: ObjectId,
+    ref: 'Post',
+  },
+  reactBy: {
+    type: ObjectId,
+    ref: 'User',
+  },
+});
+// Create reaction model
+const Reaction = model('Reaction', reactionSchema);
+
+// Export the model
+export default Reaction;
