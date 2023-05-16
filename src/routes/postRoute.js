cc00f0e336c486e975459cd47fb7c5f6911e9a20
Nitzan Tomer <Nitzantomer1998@gmail.com>
2023-05-16 08:23:23 +0300

    Adjusting notes

--- src/routes/postRoute.js
+++ src/routes/postRoute.js
@@ -1,3 +1,4 @@
+// Import needed functions
 import userMiddleware from '../middlwares/userMiddleware.js';
 import { Router } from 'express';
 import {
@@ -8,15 +9,21 @@ import {
   deletePost,
 } from '../controllers/postController.js';
 
+// Define router
 const router = Router();
 
+// Define Get routes
 router.get('/getAllPosts', userMiddleware, getAllPosts);
 
+// Define POST routes
 router.post('/createPost', userMiddleware, createPost);
 
+// Define PUT routes
 router.put('/commentPost', userMiddleware, commentPost);
 router.put('/savePost/:id', userMiddleware, savePost);
 
+// Define DELETE routes
 router.delete('/deletePost/:id', userMiddleware, deletePost);
 
+// Export the router
 export default router;
